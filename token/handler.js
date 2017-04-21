de772f1cca3f1eff2a7e1d1f3bfbbd8ba8fad269
Joel Thoms <jthoms@gmail.com>
2017-04-20 21:48:30 -0700

    added new events for testing

--- token/handler.js
+++ token/handler.js
@@ -1,5 +1,5 @@
-const callbackify = require('functional-js/promises/callbackify')
-const token = require('./token')
+const callbackify      = require('functional-js/promises/callbackify')
+const token            = require('./token')
 const withJsonResponse = require('./lib/serviceHelpers').withJsonResponse
 
 module.exports.token = callbackify((event, context) =>
