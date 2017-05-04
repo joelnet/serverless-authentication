eb7d83df81925968649bf11f47e9545e1a32150a
Joel Thoms <jthoms@gmail.com>
2017-05-03 19:22:37 -0700

    aws lambda compatibility fixes

--- handler.js
+++ handler.js
@@ -4,18 +4,10 @@ const token = require('./actions/token')
 const authorize = require('./actions/authorize')
 const withJsonResponse = require('./lib/serviceHelpers').withJsonResponse
 
-// module.exports.token = callbackify(withJsonResponse(token))
+module.exports.token = callbackify((request, context) =>
+    withJsonResponse(token)(request)
+)
 
-// module.exports.authorize = callbackify(withJsonResponse(authorize))
-
-module.exports.token = callbackify((request, context) => {
-    // console.log('request:', request)
-
-    return withJsonResponse(token)(request)
-})
-
-module.exports.authorize = callbackify((request, context) => {
-    // console.log('request:', request)
-
-    return withJsonResponse(authorize)(request)
-})
+module.exports.authorize = callbackify((request, context) =>
+    withJsonResponse(authorize)(request)
+)
