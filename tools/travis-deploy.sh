5a45633fd051c43bacdbc191f7fe926082bbcd47
Joel Thoms <jthoms@gmail.com>
2017-05-08 00:13:11 -0700

    travis-ci deployment

--- tools/travis-deploy.sh
+++ tools/travis-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_3d599e52b404_key -iv $encrypted_3d599e52b404_iv -in ../.secrets/public.key.enc -out ../.secrets/public.key2 -d
-openssl aes-256-cbc -K $encrypted_3d599e52b404_key -iv $encrypted_3d599e52b404_iv -in ../.secrets/private.key.enc -out ../.secrets/private.key2 -d
+pwd
+openssl aes-256-cbc -K $encrypted_3d599e52b404_key -iv $encrypted_3d599e52b404_iv -in .secrets/public.key.enc -out .secrets/public.key2 -d
+openssl aes-256-cbc -K $encrypted_3d599e52b404_key -iv $encrypted_3d599e52b404_iv -in .secrets/private.key.enc -out .secrets/private.key2 -d
 npm install --production
 npm run deploy:dev
