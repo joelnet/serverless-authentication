a6aa8a7f3d7438a4327cba2e3d53cc87422deb6f
Joel Thoms <jthoms@gmail.com>
2017-05-07 23:29:12 -0700

    travis-ci deployment script

---
+++ tools/travis-deploy.sh
@@ -0,0 +1,4 @@
+openssl aes-256-cbc -K $encrypted_3d599e52b404_key -iv $encrypted_3d599e52b404_iv -in .secret/private.key.enc -out .secret/private.key -d
+openssl aes-256-cbc -K $encrypted_3d599e52b404_key -iv $encrypted_3d599e52b404_iv -in .secret/public.key.enc -out .secret/public.key -d
+npm install --production
+npm run deploy:dev
