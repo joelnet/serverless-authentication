8e5a52cd24401e71889742526e7e9ca5c3fb25e1
Joel Thoms <jthoms@gmail.com>
2017-06-11 15:50:09 -0700

    move secrets.tar.enc to .secrets directory

--- tools/travis-decrypt.sh
+++ tools/travis-decrypt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd .secrets
 openssl aes-256-cbc -K $encrypted_3d599e52b404_key -iv $encrypted_3d599e52b404_iv -in secrets.tar.enc -out secrets.tar -d
 tar xvf secrets.tar
 rm secrets.tar.enc
